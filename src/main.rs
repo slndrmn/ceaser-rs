2f7d5aadafba3cdf95bfeab1d95123d2802d5aa3
salandarman <salandarman@proton.me>
2026-07-01 22:30:22 +0300

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use std::io::{self,Write};
+
+fn flush() {
+    io::stdout().flush().unwrap();
+}
+
+fn encrypt(msg:String,step:i32) -> String {
+    let mut encrypted_msg = String::new();
+    for character in msg.chars() {
+        let ascii = (character as u8) + ((step % 26) as u8);
+        if ascii > 122 {
+            encrypted_msg += &((ascii - 26) as char).to_string()
+        } else if character == ' ' {
+            encrypted_msg += " "
+        } else {
+            encrypted_msg  += &(ascii as char).to_string()
+        }
+    }
+    encrypted_msg
+}
+
+fn decrypt(msg:String) {
+    for step in 1..=26 {
+        let mut decrypted_msg = String::new();
+        for character in msg.chars() {
+            let ascii = (character as u8) + ((step % 26) as u8);
+            if ascii > 122 {
+                decrypted_msg += &((ascii - 26) as char).to_string()
+            } else if character == ' ' {
+                decrypted_msg += " "
+            } else {
+                decrypted_msg += &(ascii as char).to_string()
+            }
+        }
+        println!("{}",decrypted_msg)
+    }
+}
+
+fn main() {
+    print!("1- encrypt a message\n2- decrypt a message\n");
+    flush();
+    loop {
+        let mut desicion = String::new();
+        print!("> ");
+        flush();
+        io::stdin().read_line(&mut desicion).expect("erorororo");
+
+        if desicion.trim() == "1" {
+            let mut s = String::new();
+            let mut step = String::new();
+
+            print!("wats da message: ");
+            flush();
+            io::stdin().read_line(&mut s).expect("erorororo");
+            print!("wats da step (only integers allowed): ");
+            flush();
+            io::stdin().read_line(&mut step).expect("erorororo");
+
+            if let Ok(i) = step.trim().parse::<i32>() {
+                println!("{}",encrypt(s.trim().to_string(), i))
+            } else {
+                println!("bradar only integers allowed in dat section :rage:")
+            }
+        } else if desicion.trim() == "2" {
+            let mut s = String::new();
+
+            print!("wats da msage to decrypt: ");
+            flush();
+            io::stdin().read_line(&mut s).expect("eorororoor");
+
+            decrypt(s.trim().to_string());
+        } else {
+            println!("bradar ar yo stpid there is only 2 options amk")
+        }
+    }
+}
